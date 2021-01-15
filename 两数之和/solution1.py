dd3b4037561756b97138e093e6bb8cb66c00741c
Jay_Wu <48859030+Jay-9912@users.noreply.github.com>
2021-01-16 00:18:41 +0800

    Create solution1.py

---
+++ 两数之和/solution1.py
@@ -0,0 +1,22 @@
+#include<algorithm>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int> res(2,-1);
+        //sort(nums.begin(),nums.end());
+        for (int i=0;i<n;++i)
+        {
+            for (int j=i+1;j<n;++j)
+            {
+                if (nums[i]+nums[j]==target)
+                {
+                    res[0]=i;
+                    res[1]=j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+};
