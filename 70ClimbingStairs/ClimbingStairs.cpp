4b9b695a71805f840a683b10b25bd7206c05bd90
Jay_Wu <48859030+Jay-9912@users.noreply.github.com>
2021-02-13 16:41:17 +0800

    Create ClimbingStairs.cpp

---
+++ 70ClimbingStairs/ClimbingStairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> res(n+1,1);
+        int a=1;
+        int b=1;
+        int c=1;
+        for (int i=2;i<=n;++i)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+};
